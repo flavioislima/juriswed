67229555ac8db4a9ff4444a190e359399abbb41f
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-04-02 22:32:32 -0300

    feat: added default screens for tab navigations

---
+++ src/screens/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+export const Home = () => {
+  return (
+    <View style={styles.absoluteCenter}>
+      <Text style={styles.text}> This is Home </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  absoluteCenter: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 25,
+  },
+});
